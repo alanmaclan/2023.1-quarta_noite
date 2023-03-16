c86d03cdb2994bbe3189ab093ed2e974e5980a4c
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-03-15 22:00:37 -0300

    Primeiro projeto - Fase de criação

---
+++ RevisaoJava/src/br/uninove/telas/Principal.java
@@ -0,0 +1,10 @@
+package br.uninove.telas;
+
+public class Principal {
+    
+    public static void main(String args[]) {
+        System.out.println("Meu primeiro print!");
+        
+        //paramos aqui...
+    }
+}
